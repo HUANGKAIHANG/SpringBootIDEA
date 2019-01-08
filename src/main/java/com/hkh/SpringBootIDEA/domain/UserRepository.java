ef17a8c77f873e40257fbd1281962424afbbc36e
huangkaihang <476427479@qq.com>
2019-01-08 16:18:19 +0800

    spring data JPA的学习

--- src/main/java/com/hkh/SpringBootIDEA/domain/UserRepository.java
+++ src/main/java/com/hkh/SpringBootIDEA/domain/UserRepository.java
@@ -1,9 +1,24 @@
 package com.hkh.SpringBootIDEA.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserName(String userName);
 
     User findByUserNameOrEmail(String userName, String email);
+
+    User findTopByOrderByIdDesc();
+
+    //    @Transactional(timeout = 10)
+    @Query("select u from User u where u.nickName=?1")
+    User findByNickName(String nickName);
+
+    @Transactional
+    @Modifying
+    @Query("update User u set u.email=?1 where u.nickName=?2")
+    int modifyEmailByNickName(String email, String nickName);
+
 }
--- src/test/java/com/hkh/SpringBootIDEA/damain/UserRepositoryTests.java
+++ src/test/java/com/hkh/SpringBootIDEA/damain/UserRepositoryTests.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.text.DateFormat;
@@ -20,22 +24,47 @@ public class UserRepositoryTests {
     private UserRepository userRepository;
 
     @Test
-    public void test(){
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
-        String formattedDate = dateFormat.format(date);
+    public void test() {
+//        Date date = new Date();
+//        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+//        String formattedDate = dateFormat.format(date);
 
 //        userRepository.save(new User("userA","aaaaaa","aaa@qq.com","a",formattedDate));
 //        userRepository.save(new User("userB","bbbbbb","bbb@qq.com","b",formattedDate));
 //        userRepository.save(new User("userC","cccccc","ccc@qq.com","c",formattedDate));
 
-//        System.out.println(userRepository.findAll().size());
 //        Assert.assertEquals(3, userRepository.findAll().size());
-//
-//        System.out.println(userRepository.findByUserNameOrEmail("userB","bbb@qq.com"));
-//        System.out.println(userRepository.findByUserNameOrEmail("userB"," "));
 //        Assert.assertEquals("b", userRepository.findByUserNameOrEmail("userB","bbb@qq.com").getNickName());
 
 //        userRepository.delete(userRepository.findByUserName("userA"));
+    }
+
+    @Test
+    public void testPageQuery() throws Exception {
+        int page = 0, size = 2;
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(page, size, sort);
+        Page<User> userPage = userRepository.findAll(pageable);
+        System.out.println(userPage.getSize());
+        System.out.println(userPage.getContent().get(0).getUserName()
+        );
+    }
+
+    @Test
+    public void testLimitedQuery() {
+        System.out.println(userRepository.findTopByOrderByIdDesc().getEmail());
+    }
+
+    @Test
+    public void testCustomerSQL() {
+        String nickName = "b";
+        System.out.println(userRepository.findByNickName(nickName).getEmail());
+    }
+
+    @Test
+    public void testCustomerModifySQL() {
+        String nickName = "b";
+        String newEmail = "newbbb@qq.com";
+        System.out.println(userRepository.modifyEmailByNickName(newEmail, nickName));
     }
 }
